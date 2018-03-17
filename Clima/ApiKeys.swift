18caa205e5273ab892eea01fd311ada626b29864
marce <mikoarce@gmail.com>
2018-03-17 00:37:08 -0400

    initial commit

---
+++ Clima/ApiKeys.swift
@@ -0,0 +1,11 @@
+//
+//  ApiKeys.swift
+//  Clima
+//
+//  Created by Miko Arce on 2018-03-17.
+
+import Foundation
+
+class ApiKeys {
+    static let OPEN_WEATHER_API_KEY = ""
+}
---
+++ Clima/WeatherViewController.swift
@@ -0,0 +1,133 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Angela Yu on 23/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
+
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = ApiKeys.OPEN_WEATHER_API_KEY
+    
+
+    //TODO: Declare instance variables here
+    let locationManager = CLLocationManager()
+    
+    //Pre-linked IBOutlets
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Setup location manager
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    //Write the getWeatherData method here:
+    
+
+    
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+   
+    
+    //Write the updateWeatherData method here:
+    
+
+    
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    
+    
+    //Write the updateUIWithWeatherData method here:
+    
+    
+    
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+    
+    
+    //Write the didUpdateLocations method here:
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            if location.horizontalAccuracy > 0 { // Negative Horizontal Accuracy means invalid result.
+                locationManager.stopUpdatingLocation() //Stop updating once location is found, else burn battery.
+                print("long = \(location.coordinate.longitude), lat = \(location.coordinate.latitude)")
+                findLocation(longitude: location.coordinate.longitude, latitude: location.coordinate.latitude)
+            }
+        }
+    }
+    
+    func findLocation(longitude: CLLocationDegrees, latitude: CLLocationDegrees) -> String {
+        let long = longitude
+        let lat = latitude
+        let urlAsString = "http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(long)&appid=f7e33a62052ade2717ea82d57bd89a2b"
+        let url = URL(string: urlAsString)
+        Alamofire.request(url!, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+        return ""
+    }
+    
+    
+    //Write the didFailWithError method here:
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
+    
+    
+
+    
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    
+
+    
+    //Write the PrepareForSegue Method here
+    
+    
+    
+    
+    
+}
+
+
